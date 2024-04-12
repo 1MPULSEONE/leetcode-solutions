3fa5744d203abc6e57d4374267a919ee34cf251a
Parkhomenko Egor <54312766+1MPULSEONE@users.noreply.github.com>
2024-04-12 17:24:03 +0600

    Time: 61 ms (31.07%) | Memory: 51.3 MB (57.82%) - LeetSync

--- 908-middle-of-the-linked-list/middle-of-the-linked-list.ts
+++ 908-middle-of-the-linked-list/middle-of-the-linked-list.ts
@@ -1,23 +1,12 @@
-
-
 function middleNode(head: ListNode | null): ListNode | null {
-  let node = head;
-  let counter = 1;
-  while (node !== null) {
-    node = node.next;
-    counter++;
-  }
-  let id = counter % 2 === 0 ? counter / 2 : counter / 2 + 0.5;
-  node = head;
-  counter = 1;
-  console.log(id);
-  const result = [];
-  while (node !== null) {
-    if (counter === id) {
-      return node;
+    if (!head || !head.next) {
+        return head;
     }
-    counter++;
-    node = node.next
-  }
-  return null;
+    let fast = head;
+    let slow = head;
+    while (fast && fast.next) {
+        slow = slow.next;
+        fast = fast.next.next;
+    }
+    return slow;
 }
