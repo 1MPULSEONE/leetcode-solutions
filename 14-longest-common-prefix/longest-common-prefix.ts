4517275e43ce8447901bfa46570d2311591f4aa5
Parkhomenko Egor <54312766+1MPULSEONE@users.noreply.github.com>
2024-07-22 20:17:51 +0600

    Time: 1807 ms (5.07%) | Memory: 52.1 MB (47.71%) - LeetSync

---
+++ 14-longest-common-prefix/longest-common-prefix.ts
@@ -0,0 +1,31 @@
+function longestCommonPrefix(strs: string[]): string {
+
+    if (strs.length === 0 ) {
+        return ''
+    }
+
+    if (strs.length === 1) {
+        return strs[0]
+    }
+
+    let minLength = 999999;
+    for (let i = 0; i < strs.length; i++) {
+        if (minLength < strs[i].length) {
+            minLength = strs[i].length;
+        }
+    }
+
+    let i = 0;
+
+    while(i < minLength - 1) {
+        for (let j = 0; j < strs.length - 1; j++) {
+            if (strs[j].substring(0, i + 1) !== strs[j + 1].substring(0, i + 1)) {
+                return strs[0].substring(0, i)
+            } else {
+            }
+        }
+        i++;
+    }
+
+    return strs[0]
+};
