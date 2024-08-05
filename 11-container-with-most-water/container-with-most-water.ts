1916d7a46629e2c4cdb634f2d26f949a1d3b2502
Parkhomenko Egor <54312766+1MPULSEONE@users.noreply.github.com>
2024-08-05 17:14:14 +0600

    Time: 1050 ms (5.01%) | Memory: 66.1 MB (5.22%) - LeetSync

---
+++ 11-container-with-most-water/container-with-most-water.ts
@@ -0,0 +1,17 @@
+function maxArea(height: number[]): number {
+
+    let result = 0;
+    let i = 0
+    let j = height.length - 1;
+    while (i < j) {
+      result = Math.max(Math.min(height[i], height[j]) * (j - i), result)
+      console.log(result)
+      if (height[i] < height[j]) {
+        i++
+      } else {
+        j--
+      }
+    }
+
+    return result
+};
